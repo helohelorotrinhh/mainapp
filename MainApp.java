c13388cd8b69fb468a9bd688a31e13b4e9c0ef94
Ro Trinh <rotrinh1401@gmail.com>
2022-11-18 21:20:55 +0700

    first commit

---
+++ MainApp.java
@@ -0,0 +1,58 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		int a = 10 , b = 5;
+		System.out.println(" Tong:" + (a+b));
+		
+		System.out.println(" Hieu:" + (a-b));
+		
+		System.out.println(" Nhan:" + (a*b));
+		
+		System.out.println(" Chia:" + (a/b));
+		
+		
+		
+		
+		
+	
+		boolean c = false;
+		System.out.println(" a > b: " + ( a > b ));
+		System.out.println(" a < b: " + (a < b ));
+		System.out.println(" a <= b: " + ( a <= b));
+		System.out.println(" a >= b: " + ( a >= b));
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+	
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+	}
+}
+
+
